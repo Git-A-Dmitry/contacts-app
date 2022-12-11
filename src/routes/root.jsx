72872ad3169456dfd267733569388d06469f6843
Dmitry A <adcntt@gmail.com>
2022-12-11 17:11:56 +0300

    delete action

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, NavLink, useLoaderData, Form, redirect } from 'react-router-dom';
+import { Outlet, NavLink, useLoaderData, Form, redirect, useNavigation } from 'react-router-dom';
 import { getContacts, createContact } from '../contacts';
 
 export async function action() {
@@ -14,6 +14,7 @@ export async function loader() {
 
 function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
 
   return (
     <>
@@ -69,7 +70,7 @@ function Root() {
           )}
         </nav>
       </div>
-      <div id='detail'>
+      <div id='detail' className={navigation.state === 'loading' ? 'loading' : ''}>
         <Outlet />
       </div>
     </>
